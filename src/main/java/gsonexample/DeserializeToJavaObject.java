c6fae8568a81fb1456ff01dd209169fcfac07678
nobuyasu tomura <ntomrua92@gmail.com>
2015-11-12 01:54:59 +0900

    add 'final' modifier, and SuppressWarnings(unchecked)

--- src/main/java/gsonexample/DeserializeToJavaObject.java
+++ src/main/java/gsonexample/DeserializeToJavaObject.java
@@ -1,18 +1,17 @@
 package gsonexample;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 
 class DeserializeToJavaObject {
-
-    public static <T> T allocateInstanceWithOutConstructor(Class<T> willAllocateClass) throws ClassNotFoundException, NoSuchFieldException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
-        Field f = unsafeClass.getDeclaredField("theUnsafe");
+    
+    @SuppressWarnings("unchecked")
+    public static <T> T allocateInstanceWithOutConstructor(final Class<T> willAllocateClass) throws ClassNotFoundException, NoSuchFieldException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        final Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+        final Field f = unsafeClass.getDeclaredField("theUnsafe");
         f.setAccessible(true);
         final Object unsafe = f.get(null);
         final Method allocateInstance = unsafeClass.getMethod("allocateInstance", Class.class);
-
+        
         return (T) allocateInstance.invoke(unsafe, willAllocateClass);
     }
 }
